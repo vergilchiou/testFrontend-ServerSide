0944f14e9db3f6330dc4aada7f037e4f2a806a9c
vergilchiou <vergilchiou@gmail.com>
2025-06-21 18:14:05 +0800

    Fix filters of orders.hbs and the appearance of badges

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-# testFrontend-ServerSide
-=======
-# Frontend-ServerSide
->>>>>>> Initial commit
-Frontend-ServerSide deploy test
+# 一、專案基本
+專案名稱（Project Title）：*Little Chapter* 管理後台
+專案簡介：此為 Little Chapter 親子繪本電商平台的管理後台。
--- javascripts/all.js
+++ javascripts/all.js
@@ -1,8 +1,9 @@
 // Toggle
 const toggleMenuBtn = document.querySelector("#toggle-btn");
+
 const body = document.querySelector("body")
+
 toggleMenuBtn.addEventListener('click', (evt) => {
-  // console.log(evt);
   evt.preventDefault();
   body.classList.toggle('sidebar-toggled');
 })
